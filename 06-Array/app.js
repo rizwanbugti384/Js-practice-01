29cd87d06efc3ba5a64e045d323bd717cc2c6672
rizwanbugti384 <zongbugti11@gmail.com>
2025-10-25 16:00:19 +0500

    arry done

---
+++ 06-Array/app.js
@@ -0,0 +1,105 @@
+// console.log("hello world");
+
+// var let const
+
+// var username = "rizwanbugti"
+// var username = "asmt" // redeclare
+// username = "habib" // reassign
+// console.log(username);
+
+// let username = "rizwanbugti"
+// username = "asmt"  // reassign
+// console.log(username);
+
+// const username = "rizwanbugti";
+// console.log(username);
+
+// Array
+
+// index
+// length
+
+// let names = ["rizwan" , "asmt" , "zakriya" , "habib"]
+// console.log(names.length);
+// console.log(names[4]);
+
+// let mixedArr = [1 , 2 ,"st" , true , function (){
+//     console.log("hello world");
+//     return "username"
+
+// }]
+
+// console.log(mixedArr[4]());
+
+// array methods
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+//nested array
+
+// console.log(fruits);
+
+// fruits.push("pineapple")
+// fruits.pop()
+// fruits.unshift(["pineapple"])
+// fruits.shift()
+
+let fruits = ["apple", "mango", "banana", "strawberry", "cherry", "melon"];
+
+// fruits.splice(3 , 1 , "add krdo" , "updated")
+// fruits.splice(4 , 0 , "updated")
+// fruits.splice(2 , 2 , "updated")
+// console.log(fruits);
+
+// slice ka method array ki copy banan ka kaam ata ha.Slice ka method main array ma change nahi krta
+
+// let newArr = fruits.slice(1 , 4);
+// console.log(newArr);
+
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+// string ko array convert krna ka liya
+
+// let username = "mir rizwan bugti"
+// console.log(username.split("a"));
+
+// arr to str
+
+// console.log(fruits.join(" "));
+// console.log(fruits.reverse());
+
+// reverse a string
+
+// let username = "rizwan bugti khan"
+
+// console.log(username.split(" ").reverse().join(" "));
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [6, 7, 8, 9, 10];
+
+
+// let mergedArr = arr2.concat(arr1)
+// // console.log(mergedArr);
+// // console.log(mergedArr.sort());
+
+// console.log(mergedArr);
+// // console.log(mergedArr.indexOf(1));
+// console.log(mergedArr.includes(101));
+
+
