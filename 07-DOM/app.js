6fbbd153ea4acf17d9133d28b72cd42bdf5a08a2
rizwanbugti384 <zongbugti11@gmail.com>
2025-10-26 13:32:11 +0500

    star loop

---
+++ 07-DOM/app.js
@@ -0,0 +1,124 @@
+
+
+
+
+
+
+
+
+
+// function greetUser(username){
+//     console.log("hello " + username);
+//     return "hello " + username
+    
+// }
+
+// let greetings = greetUser("abdullah")
+// console.log(greetings);
+
+
+// console.log(greetUser("usman"));
+
+
+// const heading = document.querySelector("#head");
+// heading.innerHTML = "Changed through js"
+// // console.log(heading.innerHTML);
+
+
+
+
+// const p = document.querySelector(".para");
+
+// p.innerHTML = "testing para"
+
+
+
+// const heading = document.querySelector("h1");
+
+// heading.innerHTML = "hellow"
+
+
+// function testing(){
+//     console.log("hello world");
+    
+// }
+
+
+
+// const input = document.querySelector("#input");
+
+
+// function getVal(){
+//     console.log(input.value);
+    
+// }
+
+
+// const heading = document.querySelector("#state");
+// const btn = document.querySelector("#btn");
+
+// function changeState(){
+//     console.log(heading.innerHTML);
+//     console.log(btn.innerHTML);
+
+//     if(heading.innerHTML === "Onn"){
+//         heading.innerHTML = "Off"
+//         btn.innerHTML = "Turn on"
+//     } else {
+//         heading.innerHTML = "Onn"
+//         btn.innerHTML = "Turn off"
+//     }
+    
+// }
+
+
+// const input = document.querySelector("#input");
+// const result = document.querySelector("#result");
+
+// function greetuser(){
+//     console.log(input.value);
+//     result.innerHTML = "Hello " + input.value
+
+
+//     input.value = ""
+    
+// }
+
+
+// function clearScreen(){
+//     result.innerHTML = ""
+// }
+
+
+
+
+
+// head.innerHTML = "change through javascript"
+
+
+// const head = document.querySelector("#head");
+// const input = document.querySelector("#input");
+
+
+// function testing(){
+//     console.log(input.value);
+//     head.style.color = "white"
+//     head.style.backgroundColor = "red"
+    
+// }
+
+
+
+
+// input sa value leka neecha screen pa print krna ha
+// button ka click pa turn off and turn onn krna ha
+
+// const body = document.querySelector("body")
+
+
+// function changeColor(color){
+//     console.log(color);
+//     body.style.backgroundColor = color
+    
+
+// }
---
+++ 08- loop/app.js
@@ -0,0 +1,110 @@
+// console.log("hello Loops");
+
+
+// for, while, dowhile
+
+// initialization
+// condition check
+// increment
+
+// for(let i = 0; i < 10; i++){
+//     console.log("hello world from loop" , i);  
+// }
+
+// console.log("console after loop");
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// `` backtic string ES6 Ecma script
+
+
+// let username = "mabdullah"
+// let age = 21;
+// let city = "karachi";
+
+
+// username is (mabdullah) user age is (21) and he live in (karachi)
+
+
+// console.log("username is " + username + " user age is " + age + " live in " + city);
+
+// console.log(`username is ${username} and user age is ${age} live in ${city}`);
+
+
+
+// console.log("adsdas
+//     asd
+//     ");
+
+// console.log(`
+    
+//     asdas
+    
+    
+//     asd`);
+
+// i ka matlb iteration
+// let num = 7;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${num} x ${i} = ${num*i}`);
+    
+// }
+
+
+
+// nested loop
+
+// for(let i = 1; i <= 5; i++){
+//     console.log("outer loop i" , i);
+
+//     for(let j = 1; j <= 5; j++){
+//         console.log("inner loop j" , j);
+        
+//         for(let k = 1; k <= 3; k++){
+//             console.log("d inner k" , k);
+            
+//         }
+//     }
+    
+// }
+
+// console.log("console after loop");
+
+// let num = 0
+
+// for(let i = 1; i <= 1000; i++){
+//     console.log(i);
+//     num += 1
+
+//     for(let j = 1; j <= 20; j++){
+//         console.log(`${i} * ${j} = ${i*j}`);
+//         num += 1
+        
+//     }
+    
+// }
+
+// console.log(num);
+
+
+
+let fruit = ['apple' , 'banana' , 'orange' , 'mango']
+
+// console.log(fruit[0]);
+// console.log(fruit[1]);
+// console.log(fruit[2]);
+// console.log(fruit[3]);
+
+
+// for(let i = 0; i < fruit.length)
+
+
+
+// let sentence = "Learning Javascript is fun and rewarding!"
+
